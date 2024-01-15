983c7bffd3fca54d71811cda4c1cb6cf6e9a7256
Jomerjom <ekenesjoachim@gmail.com>
2024-01-15 15:02:18 +0100

    started creating a nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
 import Hero from './components/Hero.vue'
+import Nav from './components/Nav.vue'
+
+
 </script>
 
 <template>
+  <Nav />
  <Hero miniPitch="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed metus erat. Proin aliquet facilisis justo vel fringilla. Praesent eu tellus in dui gravida pharetra. Ut posuere purus quis neque." yourName="Joachim" imagePath="/public/avatar-placeholder.webp" />
 </template>
 
