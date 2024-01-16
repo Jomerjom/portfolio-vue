8e6a6fe9542a83555814534d6b57dbbe523a07de
Jomerjom <ekenesjoachim@gmail.com>
2024-01-16 12:00:08 +0100

    created responsive nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <script setup>
 import Hero from './components/Hero.vue'
-import Nav from './components/Nav.vue'
+import ResponsiveNav from './components/ResponsiveNav.vue';
 
 
 </script>
 
 <template>
-  <Nav />
- <Hero miniPitch="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed metus erat. Proin aliquet facilisis justo vel fringilla. Praesent eu tellus in dui gravida pharetra. Ut posuere purus quis neque." yourName="Joachim" imagePath="/public/avatar-placeholder.webp" />
+  <ResponsiveNav />
+  <main>
+  <Hero
+  miniPitch="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed metus erat. Proin aliquet facilisis justo vel fringilla. Praesent eu tellus in dui gravida pharetra. Ut posuere purus quis neque."
+  yourName="Joachim" imagePath="/public/avatar-placeholder.webp" />
+</main>
 </template>
 
 <style scoped>
+main {
+  padding: 0 1rem;
+}
 
+@media (min-width: 768px) {
+  main {
+    padding: 0 2rem;
+  }
+}
 </style>
